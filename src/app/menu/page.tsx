378e4d0228ee8558c537a1fd192e1e4b51348381
omartrese <omarlayachi13@gmail.com>
2025-09-03 19:04:53 +0200

    Add Sessions section's header

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,8 +2,21 @@ import MenuHeader from "./components/MenuHeader";
 
 export default function Menu() {
   return (
-    <MenuHeader />
+    <main>
+      <MenuHeader />
 
-    
+      <section className="flex flex-row justify-around  px-2">
+        <div className="*:leading-6">
+          <h1 className="text-xl font-extrabold text-black">My Brainstorming Sessions</h1>
+          <p className=" text-sm text-gray-500">Create or join Brainstorming Sessions</p>
+        </div>
+
+        <div>
+          <button className="btn btn-primary">
+            New session
+          </button>
+        </div>
+      </section>
+    </main>
   )
 }
