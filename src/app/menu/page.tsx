8299323c1530254a4e0beccd9727ce2820df1f16
omartrese <omarlayachi13@gmail.com>
2025-09-03 18:44:35 +0200

    Make as a menu component the MenuHeader.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,32 +1,9 @@
+import MenuHeader from "./components/MenuHeader";
+
 export default function Menu() {
   return (
-    <div className="navbar px-3 sm:px-4 md:px-6 w-full max-w-[100vw] sm:max-w-2xl md:max-w-3xl lg:max-w-4xl mx-auto">
-      <div className="flex-1">
-        <a className="text-base sm:text-lg md:text-xl lg:text-2xl text-black">daisyUI</a>
-      </div>
-      <div className="flex gap-2">
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className=" border-0 avatar hover:cursor-pointer hover:drop-shadow-md ">
-            <div className="w-9 sm:w-11 md:w-12 rounded-full">
-              <img
-                alt="User avatar"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu dropdown-content bg-base-100 rounded-box z-[100] mt-3 w-48 sm:w-56 p-2 shadow-lg text-sm sm:text-base">
-            <li>
-              <a className="justify-between py-3">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li><a className="py-3">Settings</a></li>
-            <li><a className="py-3">Logout</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <MenuHeader />
+
+    
   )
 }
