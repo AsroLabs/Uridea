977e54deafcce002c8872b715d75d3d2337c59f1
omartrese <omarlayachi13@gmail.com>
2025-09-03 17:39:39 +0200

    Install daisyui@latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ import { useEffect } from "react";
 
 export default function Index() {
 
-  useEffect(() => {
-    const getSession = async () => {
-      const { user, error } = await getUser();
-      if (user) {
-        redirect("/dashboard");
-      } else {
-        redirect("/auth");
-      }
-    };
-    getSession();
-  }, []);
+  // useEffect(() => {
+  //   const getSession = async () => {
+  //     const { user, error } = await getUser();
+  //     if (user) {
+  //       redirect("/dashboard");
+  //     } else {
+  //       redirect("/auth");
+  //     }
+  //   };
+  //   getSession();
+  // }, []);
 
   return (
     <div>Redirecting...</div>
