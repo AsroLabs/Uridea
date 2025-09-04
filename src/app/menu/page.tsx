8f6704bfd3e07f792b459c7108bf879f40896c68
omartrese <omarlayachi13@gmail.com>
2025-09-04 19:26:39 +0200

    Componetize HeroSection.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,20 +1,11 @@
+import HeroSection from "./components/HeroSection";
 import MenuHeader from "./components/MenuHeader";
 
 export default function Menu() {
   return (
     <main>
       <MenuHeader />
-
-      <section className="flex flex-col md:flex-row md:justify-around items-center gap-5 px-2 py-8">
-        <div className="text-center">
-          <h1 className="text-lg md:text-xl font-extrabold text-black leading-6 md:leading-10">My Brainstorming Sessions</h1>
-          <p className=" text-sm text-gray-500 leading-4">Create or join Brainstorming Sessions</p>
-        </div>
-
-        <button className="btn  btn-primary btn-sm md:btn-md lg:btn-lg">
-          + New session
-        </button>
-      </section>
+      <HeroSection />
     </main>
   )
 }
