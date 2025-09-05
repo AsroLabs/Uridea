1cc04a75cd16f789ed928270de476bbdf4d7bd36
omartrese <omarlayachi13@gmail.com>
2025-09-05 18:47:34 +0200

    Add some styles to every section

--- src/app/menu/components/ui/Card.tsx
+++ src/app/menu/components/ui/Card.tsx
@@ -1,11 +1,11 @@
 export default function Card({ title = "Sesión", description = "Sesión para decidir qué hacemos con este proyecto" }: { title: string; description: string }) {
     return (
-        <div className="card-xs sm:card-sm md:card-md bg-base-100 shadow-sm">
+        <div className="card-xs sm:card-sm md:card-md bg-base-100 shadow-sm rounded-2xl">
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p>{description}</p>
-                <div className="justify-end card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div className="justify-end card-actions ">
+                    <button className="btn btn-primary rounded btn-xs sm:btn-sm md:btn-md">View session</button>
                 </div>
             </div>
         </div>
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -8,7 +8,7 @@ export default function Menu() {
       <MenuHeader />
       <HeroSection />
 
-      <section className="grid grid-cols-2 gap-4 place-items-center max-w-4xl mx-auto">
+      <section className="grid grid-cols-2 gap-4 place-items-center max-w-4xl mx-auto px-2 text-balance">
         <Card title="Sesión 1" description="Sesión para decidir qué hacemos con este proyecto" />
         <Card title="Sesión 2" description="Sesión para decidir qué hacemos con este proyecto" />
         <Card title="Sesión 3" description="Sesión para decidir qué hacemos con este proyecto" />
